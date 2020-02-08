67eb68159a6be005ca0fbe6c1e381b5d8d2b0862
Joshua Colvin <joshuacolvin@Joshuas-MacBook-Pro.local>
2020-02-08 16:41:15 -0600

    Created backdrop gradient

---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+declare const init: () => void;
+declare const createGradient: () => void;
+declare const createNavigation: () => void;
--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,60 @@
-var shape = /** @class */ (function () {
-    function shape() {
-        this.name = "";
-    }
-    return shape;
-}());
-var num = 5;
-console.log(num);
+"use strict";
+const init = () => {
+    createGradient();
+    createNavigation();
+};
+const createGradient = () => {
+    console.log("createGradient Running");
+    const cvsEl = document.createElement("canvas");
+    cvsEl.setAttribute("id", "bgCanvas");
+    document.body.appendChild(cvsEl);
+    const cvs = document.getElementById("bgCanvas");
+    let ctx = cvs.getContext('2d');
+    let grd = ctx.createLinearGradient(0, 0, 300, 150);
+    // Create color stops
+    grd.addColorStop(0, "#340604");
+    grd.addColorStop(1, "black");
+    ctx.fillStyle = grd;
+    ctx.fillRect(0, 0, 300, 150);
+};
+const createNavigation = () => {
+    console.log("createNavigation Running");
+    const navigation = [
+        {
+            name: "About",
+            subdirectories: []
+        },
+        {
+            name: "Services",
+            subdirectories: [
+                {
+                    name: "Reedmaking",
+                    subdirectories: {}
+                },
+                {
+                    name: "Editing",
+                    subdirectories: {}
+                },
+                {
+                    name: "Writing",
+                    subdirectories: {}
+                },
+                {
+                    name: "Performance",
+                    subdirectories: {}
+                },
+            ]
+        },
+        {
+            name: "Listen",
+            subdirectories: []
+        },
+        {
+            name: "Contact",
+            subdirectories: []
+        }
+    ];
+    let pathName = window.location.pathname;
+    let pathArray = pathName.split("/");
+    console.log(pathArray[6]);
+};
